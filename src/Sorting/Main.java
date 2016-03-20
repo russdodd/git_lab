6ccdab2b6493d7972287cda3b70955f830745b67
Lucile Ackley <lucile_ackley@brown.edu>
2016-03-20 14:46:34 -0400

    changed println

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -20,12 +20,14 @@ public class Main {
 		}
 		System.out.println();
 		
-		System.out.println("Sorted Array:");
-		int[] sortedArray = BubbleSort.bubbleSort(unsortedArray);
+
+		System.out.println("This is the Sorted Array :");
+		int[] sortedArray = InsertionSort.insertionSort(unsortedArray);
+
 		for (int i: unsortedArray) {
 				System.out.print(i + " ");
 		}
-		//TODO: sort the array and print out its contents
+		
 		
 		
 	}
